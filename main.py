c65da46ec75e636573f418cf44929c209c1f9a5b
Prime Patel <primespatel@gmail.com>
2020-09-05 19:49:28 +0530

    added password generating function

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import random
 
+LOWER_CASES = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+UPPER_CASES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+SPECIAL_CHARS = ['!', '@', '#', '$', '&', '?', '.']
+NUMBERS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
 def pin_generator(length):
     pin = ""
     for i in range(length):
@@ -7,4 +12,14 @@ def pin_generator(length):
         pin = pin + str(digit)
     return pin
 
-print(pin_generator(5))
+# print(pin_generator(5))
+
+def pass_generator(length):
+    password = ""
+    for i in range(length):
+        lst = random.choice([LOWER_CASES, UPPER_CASES, SPECIAL_CHARS, NUMBERS])
+        char = random.choice(lst)
+        password = password + char
+    return password
+
+print(pass_generator(5))
